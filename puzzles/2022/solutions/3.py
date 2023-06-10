783e390fab97166a7319e21521833cd485b9ab5b
Sylann <romainfvincent@gmail.com>
2023-06-10 22:46:54 +0200

    ✨ solve puzzle 2022 3

---
+++ puzzles/2022/solutions/3.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import sys
+import typing as t
+
+data = sys.stdin.read()
+__ = lambda x: sys.stderr.write(f"[DEBUG] {x}\n")
+
+# ---- START OF SOLUTION -----
+
+priorities = {
+    **{chr(ord('a') + x): x + 1 for x in range(0, 26)},
+    **{chr(ord('A') + x): x + 27 for x in range(0, 26)},
+}
+
+
+def iter_common_in_rs_halves(data: str):
+    for rucksack in data.strip().split("\n"):
+        half_size = len(rucksack) // 2
+        c1, c2 = rucksack[:half_size], rucksack[half_size:]
+
+        yield set(c1) & set(c2)
+
+def iter_common_in_3_rs(data: str):
+    rucksacks = data.strip().split("\n")
+
+    for i in range(0, len(rucksacks), 3):
+        yield set(rucksacks[i]) & set(rucksacks[i + 1]) & set(rucksacks[i + 2])
+
+
+def count_priority(it: t.Iterator[set[str]]):
+    commons = []
+
+    for common in it:
+        __(f"common = {common}")
+        commons += common
+
+    return sum(priorities[c] for c in commons)
+
+
+print("1:", count_priority(iter_common_in_rs_halves(data)))
+print("2:", count_priority(iter_common_in_3_rs(data)))
