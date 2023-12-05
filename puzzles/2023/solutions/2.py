3987d5b3b49f651bff2b38656bcb06aa89142e05
Sylann <romainfvincent@gmail.com>
2023-12-05 19:47:23 +0100

    ✨ solve puzzle 2023 day 2

---
+++ puzzles/2023/solutions/2.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import sys
+
+data = sys.stdin.read()
+__ = lambda x: sys.stderr.write(f"[DEBUG] {x}\n")
+
+# ----- START OF SOLUTION -----
+
+rule = {"red": 12, "green": 13, "blue": 14}
+
+
+def iter_game_info(data: str):
+    for game_str in data.strip().split("\n"):
+        id_, sets_str = game_str.removeprefix("Game ").split(": ")
+        yield id_, list(iter_game_sets(sets_str))
+
+
+def iter_game_sets(sets_str: str):
+    game_sets = sets_str.split("; ")
+    for game_set in game_sets:
+        for x in game_set.split(", "):
+            qty, color = x.split()
+            yield int(qty), color
+
+
+def game_is_valid(game_sets: list[tuple[int, str]]):
+    for qty, color in game_sets:
+        if qty > rule[color]:
+            return False
+    return True
+
+
+def solve_1(data: str) -> int:
+    total = 0
+    for id_, game_sets in iter_game_info(data):
+        if game_is_valid(game_sets):
+            total += int(id_)
+    return total
+
+
+def compute_power(game_sets: list[tuple[int, str]]) -> int:
+    maxes = {"red": 0, "green": 0, "blue": 0}
+    for qty, color in game_sets:
+        if qty > maxes[color]:
+            maxes[color] = qty
+    return maxes["red"] * maxes["green"] * maxes["blue"]
+
+
+def solve_2(data: str) -> int:
+    total = 0
+    for id_, game_sets in iter_game_info(data):        
+        total += compute_power(game_sets)
+    return total
+
+
+print("1:", solve_1(data))
+print("2:", solve_2(data))
