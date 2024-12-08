f7c57d5c99fca3ba394dd53dbbc5791b2efc0806
Romain Vincent <romain.vincent@avisto.com>
2024-12-08 19:20:16 +0100

    perf: puzzle 2024 day 4 [go] significantly faster (at least 4x)

--- src/go/year_2024/day_04.go
+++ src/go/year_2024/day_04.go
@@ -11,80 +11,76 @@ func init() {
 	Solutions[04] = Day04{}
 }
 
-type search struct{ x0, y0, x1, y1, x2, y2, x3, y3 int }
+type search struct{ ax, ay, bx, by, cx, cy, dx, dy, w, h int }
 
-var horizontal = search{0, 0, 1, 0, 2, 0, 3, 0}
-var vertical = search{0, 0, 0, 1, 0, 2, 0, 3}
-var diagonal1 = search{0, 0, 1, 1, 2, 2, 3, 3}
-var diagonal2 = search{0, 3, 1, 2, 2, 1, 3, 0}
+var horizontal = search{0, 0, 1, 0, 2, 0, 3, 0, 4, 1}
+var vertical = search{0, 0, 0, 1, 0, 2, 0, 3, 1, 4}
+var diagonal1 = search{0, 0, 1, 1, 2, 2, 3, 3, 4, 4}
+var diagonal2 = search{0, 3, 1, 2, 2, 1, 3, 0, 4, 4}
 
-type grid []string
-
-func NewGrid(data string) grid {
-	return strings.Split(strings.TrimSpace(data), "\n")
+type grid struct {
+	// INFO: having width and height together with rows in the struct
+	// noticeably improves performance. Better memory caching?
+	rows []string
+	w, h int
 }
 
-func (g grid) Search1(s search, x, y int) int {
-	c0 := g[y+s.y0][x+s.x0]
-	c1 := g[y+s.y1][x+s.x1]
-	c2 := g[y+s.y2][x+s.x2]
-	c3 := g[y+s.y3][x+s.x3]
-	forward := c0 == 'X' && c1 == 'M' && c2 == 'A' && c3 == 'S'
-	backward := c3 == 'X' && c2 == 'M' && c1 == 'A' && c0 == 'S'
-	if forward || backward {
-		return 1
+func NewGrid(data string) grid {
+	rows := strings.Split(strings.TrimSpace(data), "\n")
+	w, h := len(rows[0]), len(rows)
+	return grid{rows, w, h}
+}
+
+// INFO: Just comparing arrays instead of multiple individual bytes
+// makes the code run much much faster.
+var XMAS = [4]byte{'X', 'M', 'A', 'S'}
+var SAMX = [4]byte{'S', 'A', 'M', 'X'}
+
+func (g grid) Search(s search) int {
+	w := g.w - s.w + 1
+	h := g.h - s.h + 1
+	total := 0
+	for y := 0; y < h; y++ {
+		// INFO: avoiding y access on each x loop is a critical performance improvement
+		ay, by, cy, dy := g.rows[y+s.ay], g.rows[y+s.by], g.rows[y+s.cy], g.rows[y+s.dy]
+		for x := 0; x < w; x++ {
+			word := [4]byte{ay[x+s.ax], by[x+s.bx], cy[x+s.cx], dy[x+s.dx]}
+			if word == XMAS || word == SAMX {
+				total++
+			}
+		}
 	}
-	return 0
+	return total
 }
 
 func (Day04) Solve1(data string) string {
-	grid := NewGrid(data)
-	w, h := len(grid[0]), len(grid)
-	out := 0
-	for y := 0; y < h; y++ {
-		for x := 0; x < w-3; x++ {
-			out += grid.Search1(horizontal, x, y)
-		}
-	}
-	for y := 0; y < h-3; y++ {
-		for x := 0; x < w; x++ {
-			out += grid.Search1(vertical, x, y)
-		}
-	}
-	for y := 0; y < h-3; y++ {
-		for x := 0; x < w-3; x++ {
-			out += grid.Search1(diagonal1, x, y) + grid.Search1(diagonal2, x, y)
-		}
-	}
-	return strconv.Itoa(out)
+	g := NewGrid(data)
+	total := g.Search(horizontal) + g.Search(vertical) + g.Search(diagonal1) + g.Search(diagonal2)
+	return strconv.Itoa(total)
 }
 
-func (g grid) Search2(x, y int) int {
-	if g[y][x] != 'A' {
-		return 0
-	}
-	a := g[y-1][x-1] //   A +-----+ B
-	b := g[y-1][x+1] //     |     |
-	c := g[y+1][x+1] //     |     |
-	d := g[y+1][x-1] //   D +-----+ C
-	ac := a == 'M' && c == 'S'
-	ca := c == 'M' && a == 'S'
-	bd := b == 'M' && d == 'S'
-	db := d == 'M' && b == 'S'
-	if ac && db || ac && bd || ca && db || ca && bd {
-		return 1
-	}
-	return 0
-}
+//   A +-----+ B
+//     |     |
+//     |     |
+//   D +-----+ C    A    B    C    D
+var MMSS = [4]byte{'M', 'M', 'S', 'S'}
+var MSSM = [4]byte{'M', 'S', 'S', 'M'}
+var SSMM = [4]byte{'S', 'S', 'M', 'M'}
+var SMMS = [4]byte{'S', 'M', 'M', 'S'}
 
 func (Day04) Solve2(data string) string {
-	grid := NewGrid(data)
-	w, h := len(grid[0]), len(grid)
-	out := 0
-	for y := 1; y < h-1; y++ {
-		for x := 1; x < w-1; x++ {
-			out += grid.Search2(x, y)
+	g := NewGrid(data)
+	total := 0
+	for y, maxY := 1, g.h-1; y < maxY; y++ {
+		row0, row1, row2 := g.rows[y-1], g.rows[y], g.rows[y+1]
+		for x, maxX := 1, g.w-1; x < maxX; x++ {
+			if row1[x] == 'A' {
+				word := [4]byte{row0[x-1], row0[x+1], row2[x+1], row2[x-1]}
+				if word == MMSS || word == MSSM || word == SSMM || word == SMMS {
+					total += 1
+				}
+			}
 		}
 	}
-	return strconv.Itoa(out)
+	return strconv.Itoa(total)
 }
